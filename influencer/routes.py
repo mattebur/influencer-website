c4958d35f431c8129635397e22a5b5cf294e2ee4
mattebur <109103905+mattebur@users.noreply.github.com>
2022-09-01 14:40:07 +0200

    Add new files and update old one

--- influencer/routes.py
+++ influencer/routes.py
@@ -6,3 +6,13 @@ from flask import render_template
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+
+@app.route('/twitter')
+def twitter_page():
+    return render_template('twitter.html')
+
+
+@app.route('/gmail')
+def gmail_page():
+    return render_template('gmail.html')
