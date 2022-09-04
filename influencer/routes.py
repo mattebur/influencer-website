a0269210c0cb60ec35d13060dbbb5e46a0c0ff39
mattebur <109103905+mattebur@users.noreply.github.com>
2022-09-04 23:07:13 +0200

    File updated

--- influencer/routes.py
+++ influencer/routes.py
@@ -1,5 +1,7 @@
 from influencer import app
-from flask import render_template
+from flask import render_template, request, flash
+from influencer.forms import GmailAccountForm
+from influencer.my_functions import SendingMails
 
 
 @app.route('/')
@@ -13,6 +15,18 @@ def twitter_page():
     return render_template('twitter.html')
 
 
-@app.route('/gmail')
+@app.route('/gmail', methods=['GET', 'POST'])
 def gmail_page():
-    return render_template('gmail.html')
+    form = GmailAccountForm()
+    if form.validate_on_submit():
+        mail_to_send = SendingMails(email_address=form.email_address.data,
+                                     password=form.password.data,
+                                     receiver=form.receiver.data,
+                                     subject=form.subject.data,
+                                     message=form.message.data)
+        if mail_to_send == 'valid':
+            flash('Message sent', category='success')
+        else:
+            flash('There were problems with sending email, try again', category='danger')
+
+    return render_template('gmail.html', form=form)
